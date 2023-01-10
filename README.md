f69026bb86a147dd61c239dd933c3feb32f1b126
audunlk <audunlk@gmail.com>
2023-01-10 21:49:28 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# js-eval README
+This extension is intended to make short evaluations from the highlighted/selected text in your Javascript File.
+After an expression you want to calculate has been selected, the result will update live on the statusbar(left bottom corner).
+
+JS EVAL is able to use both numbers and variables, even though the variables are defined outside of the selected text.
+It currently does not support arrays, object and functions, as it is intended for small evaluations and finding the latest assigned value to a selected variable.
+
+If you want to support me so I can continue developing and extending features, you can buy me a redbull here:
+https://ko-fi.com/jseval
+
+If you encounter any bugs, have any feedback on features you'd like me to implement, don't hesitate to reach out.
+
+
+
+## Features
+
+Evaluate the selected text.
+\!\[Example 1\]\(images\EXAMPLE 1.png\)
+Get variable values from outside the selected text.
+\!\[Example 2\]\(images\EXAMPLE 2.png\)
+A variable can also get its value from other variables.
+\!\[Example 3\]\(images\EXAMPLE 3.png\)
+It is able to evaluate if a variables value is assigned to an expression(Even if the expression itself contains variables).
+Selected text inline will also be evaluated correctly.
+\!\[Example 4\]\(images\EXAMPLE 4.png\)
+
+## Extension Settings
+
+Just install and the 'Result' should appear on the statusbar in the bottom left.
+
+
+**Enjoy!**
---
+++ src/extension.ts
@@ -0,0 +1,141 @@
+
+import * as vscode from 'vscode';
+import * as vm from 'vm';
+
+export function activate(context: vscode.ExtensionContext) {
+
+	let disposable = vscode.commands.registerCommand('js-eval.js-eval', () => {
+	
+      let statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 300);
+    function updateStatusBar() {
+      let editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        statusBarItem.hide();
+        return;
+      }
+      // Get the current document
+      const document = editor.document;
+
+      // Use the document to get the text of the file
+      //get text from start of document, to start of s
+      const text = document.getText(new vscode.Range(new vscode.Position(0, 0), editor.selection.start));
+
+      // Split the text into lines
+      const lines = text.split('\n');
+
+      // Get the selected text
+      const selection = editor.selection;
+
+      // Get the selected text
+      let selectedText = document.getText(selection);
+
+      // Initialize an empty map to store the variables and their values
+      const variables = new Map<string, string>();
+      //Alternative? const variables = new Map<string, {value: string, unique: boolean}>();
+
+      
+      //Make an array with all the selected text variables.
+      let selectedTextMatch = selectedText.match(/[a-zA-Z]+/g);
+
+      if (selectedTextMatch) {
+        //in order to ignore the rest of the line when an inline selection is requesting evaluation:
+        let lineLength = lines.length - 1;
+        console.log(`selectedTextMatch: ${selectedTextMatch}`);
+        for (let j = 0; j < selectedTextMatch.length; j++) {
+          console.log(`selectedTextMatch in the loop: ${selectedTextMatch[j]}`);
+          for (let i = 0; i < lineLength; i++) {
+            console.log(selectedTextMatch);
+            // Use a regular expression to match the variable declarations
+            const line = lines[i];
+            const match = line.match(/^\s*(let|const|var)\s+(\w+)\s*=\s*([^;]+)/);
+            const reDeclareMatch = line.match(/^\s*(\w+)\s*=\s*([^;]+)/);
+
+            if (match) {
+              // Extract the variable name and value
+              const [, , name, value] = match;
+               // Replace the variable names in the value with their corresponding values
+              let modifiedValue = value;
+              variables.forEach((val, varName) => {
+                modifiedValue = modifiedValue.replace(new RegExp(varName, "g"), val);
+              });
+            // Add the variable to the map, replacing any existing value
+              variables.set(name, modifiedValue);
+            }else if(reDeclareMatch){
+              const [, name, value] = reDeclareMatch;
+              // Replace the variable names in the value with their corresponding values
+              let modifiedValue = value;
+              variables.forEach((val, varName) => {
+                modifiedValue = modifiedValue.replace(new RegExp(varName, "g"), val);
+              });
+              variables.set(name, modifiedValue);
+            }
+          }
+        }
+      }
+
+      
+      //Log all the variables in the map
+      console.log(`Found variables: ${JSON.stringify([...variables])}`);
+
+      // Replace the variables in the selected text with their values
+      let modifiedText = selectedText;
+      for (const [name, value] of variables) {
+        let modifiedTextMatch = modifiedText.match(/\b[a-zA-Z]+\b/g);
+        if(modifiedTextMatch){
+          for(let i = 0; i < modifiedTextMatch.length; i++){
+            if(modifiedTextMatch[i] === name){
+              modifiedText = modifiedText.replace(new RegExp(name, 'g'), value);
+              console.log(`modification successful`);
+            }
+          }
+        }
+      }
+      console.log(`Modified text: ${modifiedText}`);
+
+      // Evaluate the modified text
+      let result;
+      try {
+        let modifiedTextFilter = modifiedText.match(/[\[\]\{\}]/g);
+        if(selectedText === ''){
+          result = 'Select code to evaluate';
+          statusBarItem.text = `${result}`;
+          statusBarItem.show();
+        }else if(modifiedTextFilter){
+          result = 'Please select variables that are not arrays, objects, or functions';
+          statusBarItem.text = `${result}`;
+          statusBarItem.show();
+        }else{
+          //Result should be sandbox value of modified text.
+          result = vm.runInNewContext(modifiedText,
+            // sandbox,
+            );
+          //result = vm.runInNewContext(result);
+          statusBarItem.text = `Result: ${result}`;
+          statusBarItem.show();
+        }
+      } catch (e) {
+        result = (e as Error).message;
+        statusBarItem.text = `Result: ${result}`;
+        statusBarItem.show();
+      }
+    }
+
+    // Get the active text editor
+    const editor = vscode.window.activeTextEditor;
+    if (editor) {
+      updateStatusBar();
+    }
+
+    // Subscribe to the 'onDidChangeTextEditorSelection' event to update the status bar when the selection changes
+    let selectionChangeDisposable = vscode.window.onDidChangeTextEditorSelection((event) => {
+      updateStatusBar();
+    });
+
+    // Dispose the event subscriptions when the command is deactivated
+    context.subscriptions.push(selectionChangeDisposable);
+  });
+
+  context.subscriptions.push(disposable);
+}
+
+export function deactivate() { }
